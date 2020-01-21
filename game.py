76157953a4242568f325777ce672af27713d06a5
Aissa EL OUAFI <aissaelouafi1994@gmail.com>
2020-01-21 15:02:28 +0100

    Use Le Wagon REST API to check if a word is valid

--- game.py
+++ game.py
@@ -1,11 +1,18 @@
 from random import choice
 import string
+import requests
+
+
 class Game:
     def __init__(self):
         self.grid = []
         for _ in range(0, 9):
             self.grid.append(choice(string.ascii_uppercase))
     def is_valid(self, word):
-            if word is '':
+
+            r = requests.get(f'https://wagon-dictionary.herokuapp.com/{word}')
+            valid_word_from_dict = r.json()
+
+            if word is '' or not valid_word_from_dict['found']:
                 return False
             return set(list(self.grid[0])) >= set(list(word))
